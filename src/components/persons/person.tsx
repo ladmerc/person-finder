939d60ef40bf1d5ca20f9a54219d6f2a10378386
Ladna Meke <ladna_mekelive@yahoo.com>
2020-11-13 21:57:21 +0100

    improve styling

--- src/components/persons/person.tsx
+++ src/components/persons/person.tsx
@@ -2,16 +2,19 @@ import * as React from "react";
 import Avatar from "./avatar";
 
 export default function Person({ person }: any) {
-  console.log({ person });
   const { name, avatar, description } = person;
 
   return (
-    <li>
+    <li className="mt-3">
       <div className="container">
         <div className="row">
-          <Avatar person={person} />
-          <h2>{name}</h2>
-          <p>{description}</p>
+          <div className="col-md-4 col-lg-4 col-xs-6">
+            <Avatar person={person} />
+          </div>
+          <div className="col-md-8 col-lg-8 col-xs-6 text-md-left">
+            <h2>{name}</h2>
+            <p>{description}</p>
+          </div>
         </div>
       </div>
     </li>
