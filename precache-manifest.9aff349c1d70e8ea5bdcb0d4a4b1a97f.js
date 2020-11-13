50c55cf6e4d4c1f9f9198952e76ce62965618467
Ladna Meke <ladna_mekelive@yahoo.com>
2020-11-13 21:59:46 +0100

    Updates

---
+++ precache-manifest.9aff349c1d70e8ea5bdcb0d4a4b1a97f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bea25d858f5386a8e531b30cf94c865f",
+    "url": "/person-finder/index.html"
+  },
+  {
+    "revision": "fcce5e2a66e0f8372681",
+    "url": "/person-finder/static/css/2.d8b722f0.chunk.css"
+  },
+  {
+    "revision": "8523afa5e028e9e54ce5",
+    "url": "/person-finder/static/css/main.657405f1.chunk.css"
+  },
+  {
+    "revision": "fcce5e2a66e0f8372681",
+    "url": "/person-finder/static/js/2.d68c2f94.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/person-finder/static/js/2.d68c2f94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8523afa5e028e9e54ce5",
+    "url": "/person-finder/static/js/main.9cde7fc8.chunk.js"
+  },
+  {
+    "revision": "d79fa7cf6c7a24b1415b",
+    "url": "/person-finder/static/js/runtime-main.024fbd50.js"
+  },
+  {
+    "revision": "326f0a790bd9f6e53ca2139c3a68ee3d",
+    "url": "/person-finder/static/media/spinner.326f0a79.svg"
+  }
+]);
